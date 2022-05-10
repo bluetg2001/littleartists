64cdb5fcce5560a9d7c966e6dedd09cd6e4d3e25
gyun <45479579+bluetg2001@users.noreply.github.com>
2022-05-10 16:43:59 +0900

    apollo graphQL 연동

--- App.js
+++ App.js
@@ -6,32 +6,53 @@ import theme from './src/utils/theme';
 import Stack from './src/navigation/Stack';
 import BottomTabNavigation from './src/components/BottomTabNavigation';
 import {NavigationContainer} from '@react-navigation/native';
+// graphQL stuff
+import {ApolloProvider, ApolloClient, InMemoryCache, gql} from '@apollo/client';
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
 
 const App = () => {
   // 로그인, 메인 부분 하단탭 숨기기 위한 state
   const [hiddenTab, setHiddenTab] = useState(false);
   const [bottomTabIndex, setBottomTabIndex] = useState(null);
 
+  //apollo
+
+  const client = new ApolloClient({
+    // 실서버
+    // uri: 'https://asia-northeast3-piano-server.cloudfunctions.net/api',
+    // 로컬서버 - 실기기에서 돌리면 안됨
+    uri: 'http://localhost:5001/piano-server/asia-northeast3/api',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <NavigationContainer>
-      <NativeBaseProvider theme={theme}>
-        <View flex={1}>
-          <View flex={6}>
-            <Stack
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <NativeBaseProvider theme={theme}>
+          <View flex={1}>
+            <View flex={6}>
+              <Stack
+                hiddenTab={hiddenTab}
+                setHiddenTab={setHiddenTab}
+                bottomTabIndex={bottomTabIndex}
+                setBottomTabIndex={setBottomTabIndex}
+              />
+            </View>
+            <BottomTabNavigation
               hiddenTab={hiddenTab}
-              setHiddenTab={setHiddenTab}
               bottomTabIndex={bottomTabIndex}
               setBottomTabIndex={setBottomTabIndex}
             />
           </View>
-          <BottomTabNavigation
-            hiddenTab={hiddenTab}
-            bottomTabIndex={bottomTabIndex}
-            setBottomTabIndex={setBottomTabIndex}
-          />
-        </View>
-      </NativeBaseProvider>
-    </NavigationContainer>
+        </NativeBaseProvider>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 export default App;
