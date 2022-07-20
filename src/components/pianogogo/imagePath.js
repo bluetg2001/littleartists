9441712de1144d6ade99658531cc73f921d77f34
gyun <45479579+bluetg2001@users.noreply.github.com>
2022-07-20 15:42:01 +0900

    배포 1.0.1 버전 - 리포트 프로필 캐릭터 추가

--- src/components/pianogogo/imagePath.js
+++ src/components/pianogogo/imagePath.js
@@ -1,25 +1,112 @@
 // e피아노고고 개인 캐릭터 이미지 경로
 
 export const imagePath = {
-  'dotted-half-note': {
+  'dotted-eight-note-crown': {
+    uri: require('../../../assets/images/epianogogo/dotted-eight-note-crown.png'),
+  },
+  'dotted-eight-note': {
+    uri: require('../../../assets/images/epianogogo/dotted-eight-note.png'),
+  },
+  'dotted-eight-rest-crown': {
+    uri: require('../../../assets/images/epianogogo/dotted-eight-rest-crown.png'),
+  },
+  'dotted-eight-rest': {
+    uri: require('../../../assets/images/epianogogo/dotted-eight-rest.png'),
+  },
+  'dotted-half-note-crown': {
     uri: require('../../../assets/images/epianogogo/dotted-half-note-crown.png'),
   },
-  'dotted-whole-note': {
+  'dotted-half-note': {
+    uri: require('../../../assets/images/epianogogo/dotted-half-note.png'),
+  },
+  'dotted-half-rest-crown': {
+    uri: require('../../../assets/images/epianogogo/dotted-half-rest-crown.png'),
+  },
+  'dotted-half-rest': {
+    uri: require('../../../assets/images/epianogogo/dotted-half-rest.png'),
+  },
+  'dotted-quarter-note-crown': {
+    uri: require('../../../assets/images/epianogogo/dotted-quarter-note-crown.png'),
+  },
+  'dotted-quarter-note': {
+    uri: require('../../../assets/images/epianogogo/dotted-quarter-note.png'),
+  },
+  'dotted-quarter-rest-crown': {
+    uri: require('../../../assets/images/epianogogo/dotted-quarter-rest-crown.png'),
+  },
+  'dotted-quarter-rest': {
+    uri: require('../../../assets/images/epianogogo/dotted-quarter-rest.png'),
+  },
+  'dotted-whole-note-crown': {
     uri: require('../../../assets/images/epianogogo/dotted-whole-note-crown.png'),
   },
-  'dotted-whole-rest': {
+  'dotted-whole-note': {
+    uri: require('../../../assets/images/epianogogo/dotted-whole-note.png'),
+  },
+  'dotted-whole-rest-crown': {
     uri: require('../../../assets/images/epianogogo/dotted-whole-rest-crown.png'),
   },
-  'half-note': {
+  'dotted-whole-rest': {
+    uri: require('../../../assets/images/epianogogo/dotted-whole-rest.png'),
+  },
+  'eight-note-crown': {
+    uri: require('../../../assets/images/epianogogo/eight-note-crown.png'),
+  },
+  'eight-note': {
+    uri: require('../../../assets/images/epianogogo/eight-note.png'),
+  },
+  'eight-rest-crown': {
+    uri: require('../../../assets/images/epianogogo/eight-rest-crown.png'),
+  },
+  'eight-rest': {
+    uri: require('../../../assets/images/epianogogo/eight-rest.png'),
+  },
+  'half-note-crown': {
     uri: require('../../../assets/images/epianogogo/half-note-crown.png'),
   },
-  'half-rest': {
+  'half-note': {
+    uri: require('../../../assets/images/epianogogo/half-note.png'),
+  },
+  'half-rest-crown': {
     uri: require('../../../assets/images/epianogogo/half-rest-crown.png'),
   },
-  'sixteenth-rest': {
+  'half-rest': {
+    uri: require('../../../assets/images/epianogogo/half-rest.png'),
+  },
+  'quarter-note-crown': {
+    uri: require('../../../assets/images/epianogogo/quarter-note-crown.png'),
+  },
+  'quarter-note': {
+    uri: require('../../../assets/images/epianogogo/quarter-note.png'),
+  },
+  'quarter-rest-crown': {
+    uri: require('../../../assets/images/epianogogo/quarter-rest-crown.png'),
+  },
+  'quarter-rest': {
+    uri: require('../../../assets/images/epianogogo/quarter-rest.png'),
+  },
+  'sixteenth-note-crown': {
+    uri: require('../../../assets/images/epianogogo/sixteenth-note-crown.png'),
+  },
+  'sixteenth-note': {
+    uri: require('../../../assets/images/epianogogo/sixteenth-note.png'),
+  },
+  'sixteenth-rest-crown': {
     uri: require('../../../assets/images/epianogogo/sixteenth-rest-crown.png'),
   },
-  'whole-rest': {
+  'sixteenth-rest': {
+    uri: require('../../../assets/images/epianogogo/sixteenth-rest.png'),
+  },
+  'whole-note-crown': {
+    uri: require('../../../assets/images/epianogogo/whole-note-crown.png'),
+  },
+  'whole-note': {
+    uri: require('../../../assets/images/epianogogo/whole-note.png'),
+  },
+  'whole-rest-crown': {
     uri: require('../../../assets/images/epianogogo/whole-rest-crown.png'),
+  },
+  'whole-rest': {
+    uri: require('../../../assets/images/epianogogo/whole-rest.png'),
   },
 };
